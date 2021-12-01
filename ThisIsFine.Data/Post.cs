ed63ce76a11f8cbfa756ab72a43af506f8a3c0b9
Meaghan Ziegler <meaghan.ziegler@gmail.com>
2021-11-30 19:22:14 -0500

    Meaghan commit 5?

---
+++ ThisIsFine.Data/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisIsFine.Data
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+
+        //Virtual list of comments
+
+        [Required]
+        public Guid AuthorId { get; set; }
+    }
+}
