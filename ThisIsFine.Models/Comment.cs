dc5f656abaa811550475b59985a302e7ecbcad37
Jason Adams <adams.jason.029@gmail.com>
2021-11-30 22:22:29 -0500

    updated jason branch

---
+++ ThisIsFine.Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisIsFine.Data
+{
+    public class Comment
+    {
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public Guid AuthorID { get; set; }
+        public virtual List<Replies> Replies { get; set; }
+        [ForeignKey]
+    }
+}
